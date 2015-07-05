7039aa8f48ab664b817acd26cebef9752afb029b
Fabien Alfred <alfred.fabien@gmail.com>
2015-07-05 19:15:41 +0200

    [tri_bulle] html and js files edited
    html : 2 table fields + 'Générer' and 'Trier' buttons
    js : functions generateTable(), fillTable(), displayTable()

--- WebContent/javascript/tri_bulle.js
+++ WebContent/javascript/tri_bulle.js
@@ -1,3 +1,27 @@
-/**
- * 
- */
+function generateTable(){
+	"use strict"
+	var tableLength = 9;
+	var tab = fillTable(tableLength);
+	displayTable(tab, tableLength);
+}
+
+function fillTable(tableLength){
+	"use strict"
+	var tab = [];
+	for(var i=0 ; i<tableLength ; i++){
+		tab[i] = Math.floor((Math.random()*10)+1);
+	}
+	 return tab;
+}
+
+function displayTable(tab, tableLength){
+	"use strict"
+	for(var i=0 ; i<tableLength ; i++){
+		document.getElementById("idInitNum"+(i+1)).innerHTML = tab[i];
+	}
+}
+
+
+window.onload = function(){
+	document.getElementById("idGenerateTab").onclick = generateTable;
+}
