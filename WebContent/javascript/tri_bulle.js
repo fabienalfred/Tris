bde37ee98f6b735ca084ea531abe944fd728bbc2
Fabien Alfred <alfred.fabien@gmail.com>
2015-07-05 20:34:17 +0200

    [tri_bulle] table can be generated and then sorted
    html : corrected numbers of span id's
    
    js : variables tableLength, tab1, tab2 as global
    	functions sortTable(), inverseTable()
    	completion of window.onload

--- WebContent/javascript/tri_bulle.js
+++ WebContent/javascript/tri_bulle.js
@@ -1,27 +1,60 @@
+var tableLength = 9;
+var tab1 = [];
+var tab2 = [];
+
+
 function generateTable(){
 	"use strict"
-	var tableLength = 9;
-	var tab = fillTable(tableLength);
-	displayTable(tab, tableLength);
+	tab1 = fillTable(tableLength);
+	displayTable(tab1, "idInitNum");
 }
 
-function fillTable(tableLength){
+function fillTable(){
 	"use strict"
-	var tab = [];
+	var tab1 = [];
 	for(var i=0 ; i<tableLength ; i++){
-		tab[i] = Math.floor((Math.random()*10)+1);
+		tab1[i] = Math.floor((Math.random()*10)+1);
 	}
-	 return tab;
+	 return tab1;
 }
 
-function displayTable(tab, tableLength){
+function displayTable(tab, idNum){
 	"use strict"
 	for(var i=0 ; i<tableLength ; i++){
-		document.getElementById("idInitNum"+(i+1)).innerHTML = tab[i];
+		document.getElementById(idNum+(i+1)).innerHTML = tab[i];
 	}
 }
 
+function sortTable(){
+	"use strict"
+	var pointer;
+	for(var k=0 ; k<tableLength-1 ; k++){
+		pointer = 0;
+		while(pointer<=(tableLength-k)){
+			if(tab1[pointer]>tab1[pointer+1])
+				inverse(pointer);
+			pointer++;
+		}
+	}
+	displayTable(tab2, "idSortedNum");
+}
+
+function inverse(pointer){
+	"use strict"
+	for(var i=0 ; i<tableLength ; i++){
+		if(i==pointer)
+			tab2[i] = tab1[pointer+1];
+		else if(i==pointer+1)
+			tab2[i] = tab1[pointer];
+		else
+			tab2[i] = tab1[i];
+	}
+	for(var i=0 ; i<tableLength ; i++){
+		tab1[i] = tab2[i];
+	}
+}
 
 window.onload = function(){
 	document.getElementById("idGenerateTab").onclick = generateTable;
+	document.getElementById("idSortTab").onclick = sortTable;
 }
